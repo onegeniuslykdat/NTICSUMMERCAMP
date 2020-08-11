1c191f3961fcc4f27d9efb35ab58a965d80b19a4
onegeniuslykdat <27788258+onegeniuslykdat@users.noreply.github.com>
2020-08-11 15:46:51 +0100

    Update summercamp.js

--- summercamp.js
+++ summercamp.js
@@ -49,7 +49,7 @@ function Activate() {
             }
             break;
             case 2:
-                if (hr == 14/*10 && hr < 11*/) {
+                if (hr == 15/*10 && hr < 11*/) {
                     document.querySelector('table#cat1programs td#Tues1').innerHTML = 'FolkLore <a target="_blank" href="http://'+zoomclass2+'" id="JC"><button>JOIN CLASS</button></a>';
                     document.querySelector('table#cat2programs td#Tues1').innerHTML = 'FolkLore <a target="_blank" href="http://'+zoomclass2+'" id="JC"><button>JOIN CLASS</button></a>';
                     document.querySelector('table#cat3programs td#Tues1').innerHTML = 'Folklore <a target="_blank" href="http://'+zoomclass2+'" id="JC"><button>JOIN CLASS</button></a>';
