be01dc724ba15d48469faf48649a81c8c6902104
onegeniuslykdat <27788258+onegeniuslykdat@users.noreply.github.com>
2020-08-19 18:16:20 +0100

    Update summercamp1.js

--- summercamp1.js
+++ summercamp1.js
@@ -118,7 +118,7 @@ function Activate() {
                     //document.querySelector('table#cat2programs td#Wed3').style.backgroundColor = 'lightgreen'; 
                     //document.querySelector('table#cat3programs td#Wed3').style.backgroundColor = 'lightgreen';  
                 }
-                else if ((hr == 17 && min >= 39) || (hr == 18)) {
+                else if ((hr == 12 && min >= 52) || (hr == 13)) {
                     document.querySelector('table#cat1programs td#Wed4').innerHTML = 'Word Skill <a target="_blank" href="http://'+zoomclass2+'" id="JC"><button>JOIN CLASS</button></a>';
                     //document.querySelector('table#cat2programs td#Wed4').innerHTML = 'Literary Artistry <a target="_blank" href="http://'+zoomclass1+'" id="JC"><button>JOIN CLASS</button></a>';
                     //document.querySelector('table#cat3programs td#Wed4').innerHTML = 'Word Skill <a target="_blank" href="http://'+zoomclass1+'" id="JC"><button>JOIN CLASS</button></a>';
@@ -156,7 +156,7 @@ function Activate() {
                         //document.querySelector('table#cat2programs td#Thur3').style.backgroundColor = 'lightgreen';
                         //document.querySelector('table#cat3programs td#Thur3').style.backgroundColor = 'lightgreen';
                     }
-                    else if (hr == 12 && min >= 52 || hr == 13) {
+                    else if ((hr == 12 && min >= 52) || (hr == 13)) {
                         //document.querySelector('table#cat1programs td#Thur4').innerHTML = 'Literary Artistry <a target="_blank" href="http://'+zoomclass1+'" id="JC"><button>JOIN CLASS</button></a>'; 
                         //document.querySelector('table#cat2programs td#Thur4').innerHTML = 'Word Skill <a target="_blank" href="http://'+zoomclass2+'" id="JC"><button>JOIN CLASS</button></a>'; 
                         //document.querySelector('table#cat3programs td#Thur4').innerHTML = 'Literary Artistry <a target="_blank" href="http://'+zoomclass1+'" id="JC"><button>JOIN CLASS</button></a>'; 
@@ -192,7 +192,7 @@ function Activate() {
                             //document.querySelector('table#cat2programs td#Fri3').style.backgroundColor = 'lightgreen';
                             //document.querySelector('table#cat3programs td#Fri3').style.backgroundColor = 'lightgreen';  
                         }
-                        else if (hr == 12 && min >= 52 || hr == 13) {
+                        else if ((hr == 12 && min >= 52) || (hr == 13)) {
                             document.querySelector('table#cat1programs td#Fri4').innerHTML = 'Story Telling or Special Talent <a target="_blank" href="http://'+zoomclass1+'" id="JC"><button>JOIN CLASS</button></a>'; 
                             //document.querySelector('table#cat2programs td#Fri4').innerHTML = 'Story Telling or Special Talent <a target="_blank" href="http://'+zoomclass1+'" id="JC"><button>JOIN CLASS</button></a>'; 
                             //document.querySelector('table#cat3programs td#Fri4').innerHTML = 'Story Telling or Special Talent <a target="_blank" href="http://'+zoomclass1+'" id="JC"><button>JOIN CLASS</button></a>'; 
